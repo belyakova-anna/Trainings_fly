e67ef344a490bc1f4235d95d6b89c7f0238b7678
belyakova-anna <107809326+belyakova-anna@users.noreply.github.com>
2023-02-10 18:20:34 +0300

    ok2

--- main.py
+++ main.py
@@ -16,5 +16,6 @@ def index(username):
 def training(prof):
     return render_template('index.html', title='', profession=prof)
 
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
